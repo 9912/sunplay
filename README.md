7febf8737eefa1bb3f3e8108b21eab8b944f85fc
9912 <arist.9912@gmail.com>
2022-04-29 17:56:48 -0500

    v1.3 Stable + Searchable selector + SunVox DLL v2.0e

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ NOTE: The free version of AIDE only compiles, to circumvent the purchase to unlo
 ## However...
 You must create this folder in order to put the songs (*.sunvox files) onto this path:
 ```
-	(Phone Storage)/sunvoxfiles
+(Phone Storage)/sunvoxfiles
 ```
 to make them playable, once the files is stored there, you can listen them.
 
@@ -22,22 +22,30 @@ to make them playable, once the files is stored there, you can listen them.
 
 The four basic buttons are present: Play, Stop, Previous(Prev) and Next, an additional text field is present to input the offset of the song to be played.
 
+Below the current song playing, there is a searchable selector with the listed files for easy selection.
+
+Also, there is a Settings section where you can change the frame rate and the buffer.
+Take account that frame rate value must be 44100 or more.
+Is recommended the following values for the buffer: 128, 256, 512, 1024, 2048, 4096. You can insert other value by your own risk.
+
 # To Do:
 
 - [x] <del>Create Proyect</del>
 - [x] <del>App Icon</del>
 - [x] <del>Load from folder on external storage</del>
 - [x] <del>Changeable frame rate and buffer</del>
+- [x] <del>Searchable Playlist Spinner</del>
 
 # Changelog
 
+- v1.3 Stable, Added an searchable spinner to select a song, SunVox updated to v2.0e
 - v1.2 Beta stage, now you can change frame rate and buffer if you want to tweak (by your own responsibility)
 - v1.1 Beta stage, now you can put your files onto your phone storage, packing songs into the APK is no longer needed, SunVox DLL updated to v2.0c
 - v1.0 Alpha stage, packing songs on the APK is required
 
 # Disclaimer
 
-App in beta stage, some devices can get stuck or freeze with complex modules due of OpenSL implementation of the device, follow optimization tips on [SunVox forums](https://warmplace.ru/forum/viewtopic.php?f=3&t=2379&p=7730)
+<del>App in beta stage</del> Now the app is stable, but some devices can get stuck or freeze with complex modules due of OpenSL implementation of the device, in this case, modify the modules with SunVox following optimization tips on [SunVox forums](https://warmplace.ru/forum/viewtopic.php?f=3&t=2379&p=7730)
 
 Powered by [SunVox](https://warmplace.ru/soft/sunvox) (modular synth & tracker)
 Copyright (c) 2008 - 2022, Alexander Zolotov <nightradio@gmail.com>, [WarmPlace.ru](https://warmplace.ru)
--- app/src/main/java/net/arist/sunplay/MainActivity.java
+++ app/src/main/java/net/arist/sunplay/MainActivity.java
@@ -17,12 +17,17 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.content.res.Resources.NotFoundException;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.os.Process;
 import android.os.Environment;
+import android.text.*;
 
 import nightradio.sunvoxlib.SunVoxLib;
 import android.widget.*;
@@ -40,6 +45,8 @@ public class MainActivity extends Activity {
 	String baseDir;
 	int song_index = 0;
 	TextView song_title;
+	TextView selecter;
+	Dialog dialog;
 	Context context;
 	String cfg = "";
 	int newfreq;
@@ -90,6 +97,7 @@ public class MainActivity extends Activity {
 		song_title.findViewById(R.id.songtitle);
 		context = getApplicationContext();
 		baseDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+		Log.v( "PATH", baseDir );
         ArrayList<String> result = new ArrayList<String>(); //ArrayList cause you don't know how many files there is
         File folder = new File(baseDir + "/sunvoxfiles"); //This is just to cast to a File type since you pass it as a String
         File[] filesInFolder = folder.listFiles(); // This returns all the folders and files in your path
@@ -101,6 +109,95 @@ public class MainActivity extends Activity {
         svfiles = result.toArray(new String[0]);
         Arrays.sort(svfiles);
 		((TextView) findViewById(R.id.songtitle)).setText(svfiles[0]);
+		
+		selecter = findViewById(R.id.selecter);
+		selecter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Initialize dialog
+                dialog=new Dialog(MainActivity.this);
+ 
+                // set custom dialog
+                dialog.setContentView(R.layout.dialog_searchable_spinner);
+ 
+                // set custom height and width
+                dialog.getWindow().setLayout(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+ 
+                // set transparent background
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+ 
+                // show dialog
+                dialog.show();
+ 
+                // Initialize and assign variable
+                EditText editText=dialog.findViewById(R.id.edit_text);
+                ListView listView=dialog.findViewById(R.id.list_view);
+ 
+                // Initialize array adapter
+                final ArrayAdapter<String> adapter=new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,Arrays.asList(svfiles));
+ 
+                // set adapter
+                listView.setAdapter(adapter);
+                editText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void afterTextChanged(Editable s) {
+ 
+                    }
+                    
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+ 
+                    }
+ 
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        adapter.getFilter().filter(s);
+                    }
+                });
+ 
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // when item selected from list
+                        // set selected item on textView
+						svsong = svfiles[position];
+                        ((TextView) findViewById(R.id.songtitle)).setText(adapter.getItem(position));
+						
+						if( sunvox_version > 0 )
+						{
+							SunVoxLib.stop( 0 );
+						}
+						
+						int rv = SunVoxLib.load( 0, baseDir + "/sunvoxfiles/" + svsong );
+						if( rv == 0 )
+							Log.i( "SunVoxPlayer", "Song loaded" );
+						else
+							Log.e( "SunVoxPlayer", "Song load error " + rv + " " + baseDir + "/sunvoxfiles/" + svsong );        
+						
+						if(((EditText)findViewById(R.id.Offset)).getText().toString().matches("")){
+							fileoffset = 0;
+						}else{
+							fileoffset = Integer.parseInt(((EditText)findViewById(R.id.Offset)).getText().toString());
+						}
+						if( sunvox_version > 0 )
+						{
+							//Disable autostop:
+							SunVoxLib.set_autostop( 0, 0 );
+				
+							//Rewind (go to the offset):
+							SunVoxLib.rewind( 0, fileoffset );
+				
+							//Set volume:
+							SunVoxLib.volume( 0, 256 );
+							SunVoxLib.play( 0 );	        
+						}
+						
+                        // Dismiss dialog
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
 
 		sunvox_version = SunVoxLib.init( cfg, sample_rate, 2, 0 );
 		if( sunvox_version > 0 )
@@ -247,7 +344,7 @@ public class MainActivity extends Activity {
 	        SunVoxLib.play( 0 );	        
 		}
 	}
-
+	
 	public void SetConfig(View view){
 		
         if( sunvox_version > 0 )
